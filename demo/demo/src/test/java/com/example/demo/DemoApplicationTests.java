987faaf72bc2d65141cb76257e70d771a524a1cb
Ensar Iljazi <iljaziensar@gmail.com>
2022-01-16 13:10:20 +0100

    Ensar Iljazi

---
+++ demo/demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		//Customers customers = new Customers(1,"Kadri","Gashi","123124141",10.00);
+
+	}
+
+}
